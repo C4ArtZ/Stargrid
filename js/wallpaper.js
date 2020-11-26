de00cb4aef8470d10adc89d7151aba86c773407c
C4ArtZ <dev@c4artz.de>
2020-11-26 14:35:17 +0100

    Allowed for custom wallpapers by adding them to settings.js. Added drop shadow to icons to make them more visible.

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -6,19 +6,19 @@ function setwallpaper()
     var d = new Date();
       var n = d.getHours();
         if (n > 20 || n < 7){
-            root.style.setProperty('--wp', "url(./wallpaper/night.jpg)");
+            root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_night})`);
         }
 
         else if (n > 6 && n < 11){
-            root.style.setProperty('--wp', "url(./wallpaper/sunrise.jpg)");
+            root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_sunrise})`);
         }
 
         else if (n > 16 && n < 21){
-            root.style.setProperty('--wp', "url(./wallpaper/sunset.jpg)");
+            root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_sunset})`);
         }
 
         else {
-            root.style.setProperty('--wp', "url(./wallpaper/day.jpg)");
+            root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_day})`);
         }      
 }
 
