07154aaa3c14712a10bb97c9f301709ecdc93de9
C4ArtZ <dev@c4artz.de>
2020-11-27 22:22:34 +0100

    Rewrite of weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,67 +1,41 @@
 
-/*
-
-ORIGINAL CREDIT TO https://github.com/MiguelRAvila
-
-Modified by me
-
-
-Has to be reaplaced by my own code.
-
-*/
-
+// Get weather elements from html
 const iconElement = document.getElementById('weather-icon');
 const tempElement = document.getElementById('weather-temp');
-const descElement = document.getElementById('weather-desc');
 
-// App data
+// set Kelvin in Celsius
+const KELVIN = 273.15;
+
+//set unit of mesarument to celsius
 const weather = {};
 weather.temperature = {
     unit: 'celsius',
 };
 
-// Change to 'F' for Fahrenheit
-var tempUnit = 'C';
-
-const KELVIN = 273.15;
-const key = weather_apikey;
-
-// Set Position function
-setPosition();
-
-function setPosition(position) {
-    let latitude = weather_latitude;
-    let longitude = weather_longitude;
-
-    getWeather(latitude, longitude);
-}
+//Call function
+getWeather();
 
 // Get the Weather data
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${weather_latitude}&lon=${weather_longitude}&appid=${weather_apikey}`;
 
-    console.log(api);
-
-    fetch(api)
-        .then(function (response) {
+    fetch(api) // Call api
+        .then(function (response) { //Return api response as json
             let data = response.json();
             return data;
-            alert(data);
         })
-        .then(function (data) {
-            let celsius = Math.floor(data.main.temp - KELVIN);
-            weather.temperature.value = (tempUnit == 'C') ? celsius : (celsius * 9/5) + 32;
+        .then(function (data) { // Write values to object
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
             weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
         })
         .then(function () {
-            displayWeather();
+            displayWeather(); // call function to display info
         });
 }
 
-// Display Weather info
+// Display Weather
 function displayWeather() {
     iconElement.src = `icons/weather/${weather.iconId}.png`;
     tempElement.innerText = `${weather.temperature.value} °C`;
-    //descElement.innerHTML = weather.description;
 }
