60a72e6c0cb770e524845b9e0435f26f60909270
C4ArtZ <dev@c4artz.de>
2020-11-27 11:07:54 +0100

    Made dynamic wallpaper optional

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -3,23 +3,27 @@ window.onload = setwallpaper();
 function setwallpaper()
 {
     let root = document.documentElement;
-    var d = new Date();
-      var n = d.getHours();
+    if (wallpaper_dynamic == true){
+        var d = new Date();
+        var n = d.getHours();
         if (n > 20 || n < 7){
             root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_night})`);
         }
-
+        
         else if (n > 6 && n < 11){
             root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_sunrise})`);
         }
-
+        
         else if (n > 16 && n < 21){
             root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_sunset})`);
         }
-
+        
         else {
             root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_day})`);
         }      
+    } else {
+        root.style.setProperty('--wp', `url(./wallpaper/${wallpaper_default})`);
+    }
 }
 
 
